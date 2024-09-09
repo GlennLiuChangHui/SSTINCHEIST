8eb8ed425541df189023bdba5a50b16dae4e2020
123rlSSTincbcYEs <lim_zhi_chen@s2024.ssts.edu.sg>
2024-09-09 17:16:55 +0800

    Initial Commit

---
+++ project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project
+//
+//  Created by Lim Zhi Chen on 09/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Lim Zhi Chen on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
